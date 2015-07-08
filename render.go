d763a5ab8b6b640219bf6e1e8287b1e473251c25
caixw <xnotepad@gmail.com>
2015-07-08 21:22:11 +0800

    改变RenderJson在输出空值时的行为。现在不再输出空字符串，而是表示一个空对象的"{}"json字符串

--- render.go
+++ render.go
@@ -11,6 +11,7 @@ import (
 
 // 将v转换成json数据并写入到w中。code为服务端返回的代码。
 // 若v的值是string,[]byte,[]rune则直接转换成字符串写入w。
+// 当v值为nil, "", []byte(""), []rune("")等值时，将输出表示空对象的json字符串："{}"。
 // header用于指定额外的Header信息，若传递nil，则表示没有。
 func RenderJson(w http.ResponseWriter, code int, v interface{}, header map[string]string) {
 	if w == nil {
@@ -45,6 +46,9 @@ func RenderJson(w http.ResponseWriter, code int, v interface{}, header map[strin
 	}
 
 	w.WriteHeader(code)
+	if len(data) == 0 {
+		data = []byte("{}")
+	}
 	if _, err = w.Write(data); err != nil {
 		Error(err)
 	}
