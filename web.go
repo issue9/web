3c2c2d4b07791f17b349943a0d2c09d0daa5c052
caixw <xnotepad@gmail.com>
2015-10-19 22:02:56 +0800

    添加pprof配置项。
    为Config添加Pprof配置项，用于指net/http/pprof中的调试项基地址，当此值为空时，表示不指定pprof；
    当Config.Pprof的值为"/debug/pprof/"时，为作以下映射：
    /debug/pprof/cmdline ==> pprof.Cmdline;
    /debug/pprof/profile ==> pprof.Profile;
    /debug/pprof/symbol  ==> pprof.Symbol;
    /debug/pprof/trace   ==> pprof.Trace;
    /debug/pprof/        ==> pprof.Index;

--- web.go
+++ web.go
@@ -12,39 +12,25 @@ package web
 
 import (
 	"net/http"
+	"net/http/pprof"
 	"path/filepath"
+	"strings"
 
 	"github.com/issue9/context"
 	"github.com/issue9/mux"
+	"github.com/issue9/term/colors"
 )
 
 // web包的相关配置内容。
 type Config struct {
-	HTTPS      bool              `json:"https"`            // 是否启用https
-	CertFile   string            `json:"certFile"`         // 当https为true时，此值为必须
-	KeyFile    string            `json:"keyFile"`          // 当https为true时，此值为必须
-	Port       string            `json:"port"`             // 端口，不指定，默认为80或是443
-	ServerName string            `json:"serverName"`       // 响应头的server变量，为空时，不输出该内容
-	Static     map[string]string `json:"static,omitempty"` // 静态路由映身，键名表示路由路径，键值表示文件目录
-	ErrHandler mux.RecoverFunc   `json:"-"`                // 错误处理
-}
-
-// 初始化web包的内容。
-func Run(cfg *Config) {
-	checkConfig(cfg)
-
-	if len(cfg.Static) > 0 {
-		group, err := NewModule("static")
-		if err != nil {
-			panic(err)
-		}
-
-		for url, dir := range cfg.Static {
-			group.Get(url, http.StripPrefix(url, http.FileServer(http.Dir(dir))))
-		}
-	}
-
-	listen(cfg)
+	HTTPS      bool              `json:"https"`                // 是否启用https
+	CertFile   string            `json:"certFile,omitempty"`   // 当https为true时，此值为必须
+	KeyFile    string            `json:"keyFile,omitempty"`    // 当https为true时，此值为必须
+	Port       string            `json:"port,omitempty"`       // 端口，不指定，默认为80或是443
+	ServerName string            `json:"serverName,omitempty"` // 响应头的server变量，为空时，不输出该内容
+	Static     map[string]string `json:"static,omitempty"`     // 静态路由映身，键名表示路由路径，键值表示文件目录
+	Pprof      string            `json:"pprof,omitempty"`      // 指定pprof地址
+	ErrHandler mux.RecoverFunc   `json:"-"`                    // 错误处理
 }
 
 // 检测cfg的各项字段是否合法，
@@ -71,18 +57,65 @@ func checkConfig(cfg *Config) {
 	}
 }
 
+// 初始化web包的内容。
+func Run(cfg *Config) {
+	checkConfig(cfg)
+
+	if len(cfg.Static) > 0 {
+		group, err := NewModule("static")
+		if err != nil {
+			panic(err)
+		}
+
+		for url, dir := range cfg.Static {
+			group.Get(url, http.StripPrefix(url, http.FileServer(http.Dir(dir))))
+		}
+	}
+
+	listen(cfg)
+}
+
 // 开始监听。
 // errorHandler 为错误处理函数。
 func listen(cfg *Config) {
-	hf := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		if len(cfg.ServerName) > 0 {
-			w.Header().Add("Server", cfg.ServerName) // 添加serverName
-		}
+	var h http.Handler
 
-		serveMux.ServeHTTP(w, req)
-	})
+	if len(cfg.ServerName) > 0 {
+		h = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Add("Server", cfg.ServerName)
+			context.FreeHandler(serveMux).ServeHTTP(w, r)
+		})
+	}
 
-	h := context.FreeHandler(hf)
+	if len(cfg.Pprof) > 0 {
+		colors.Println(colors.Stdout, colors.Green, colors.Default, "开启了调试功能，地址为：", cfg.Pprof)
+
+		h = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if !strings.HasPrefix(r.URL.Path, cfg.Pprof) {
+				context.FreeHandler(serveMux).ServeHTTP(w, r)
+				return
+			}
+
+			path := r.URL.Path[len(cfg.Pprof):]
+			switch path {
+			case "cmdline":
+				pprof.Cmdline(w, r)
+			case "profile":
+				pprof.Profile(w, r)
+			case "symbol":
+				pprof.Symbol(w, r)
+			case "trace":
+				pprof.Trace(w, r)
+			default:
+				pprof.Index(w, r)
+			}
+		})
+	}
+
+	if h == nil {
+		h = context.FreeHandler(serveMux)
+	}
+
 	if cfg.HTTPS {
 		http.ListenAndServeTLS(cfg.Port, cfg.CertFile, cfg.KeyFile, mux.NewRecovery(h, cfg.ErrHandler))
 	} else {
