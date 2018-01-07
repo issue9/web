919e4ed0908285d3389258582751865ca29bf8ed
caixw <xnotepad@gmail.com>
2018-01-07 20:51:52 +0800

    添加 Params 对象

---
+++ context/params.go
@@ -0,0 +1,251 @@
+// Copyright 2016 by caixw, All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+package context
+
+import (
+	"errors"
+	"strconv"
+
+	"github.com/issue9/mux"
+	"github.com/issue9/mux/params"
+)
+
+var emptyParams = params.Params(map[string]string{})
+
+// Params 用于处理路径中包含的参数。
+//  p := ctx.Params()
+//  aid := p.Int64("aid")
+//  bid := p.Int64("bid")
+//  if p.HasErrors() {
+//      // do something
+//      return
+//  }
+type Params struct {
+	ctx    *Context
+	params params.Params
+	errors map[string]string
+}
+
+// Params 声明一个新的 Params 实例
+func (ctx *Context) Params() *Params {
+	params := mux.Params(ctx.Request())
+	if params == nil {
+		params = emptyParams
+	}
+
+	return &Params{
+		ctx:    ctx,
+		params: params,
+		errors: make(map[string]string, len(params)),
+	}
+}
+
+// ID 获取参数 key 所代表的值，并转换成 int 且值必须大于 0。
+func (p *Params) ID(key string) int64 {
+	id := p.Int64(key)
+	if id <= 0 {
+		p.errors[key] = "必须大于 0"
+	}
+
+	return id
+}
+
+// MustID 获取参数 key 所代表的值，转换成 int64 且必须大于 0。
+// 若不存在或是转换出错，则返回 def 作为其默认值。
+// 仅在类型转换出错或是小于零时，才会向 errors 写入错误信息。
+func (p *Params) MustID(key string, def int64) int64 {
+	str, found := p.params[key]
+
+	// 不存在，仅返回默认值，不算错误
+	if !found {
+		return def
+	}
+
+	ret, err := strconv.ParseInt(str, 10, 64)
+	if err != nil {
+		p.errors[key] = err.Error()
+		return def
+	}
+
+	if ret <= 0 {
+		p.errors[key] = "必须大于 0"
+		return def
+	}
+
+	return ret
+}
+
+// Int 获取参数 key 所代表的值，并转换成 int。
+func (p *Params) Int(key string) int {
+	return int(p.Int64(key))
+}
+
+// MustInt 获取参数 key 所代表的值，并转换成 int。
+// 若不存在或是转换出错，则返回 def 作为其默认值。
+// 仅在类型转换出错时，才会向 errors 写入错误信息。
+func (p *Params) MustInt(key string, def int) int {
+	return int(p.MustInt64(key, int64(def)))
+}
+
+// Int64 获取参数 key 所代表的值，并转换成 int64。
+func (p *Params) Int64(key string) int64 {
+	ret, err := p.params.Int(key)
+	if err != nil {
+		p.errors[key] = err.Error()
+	}
+
+	return ret
+}
+
+// MustInt64 获取参数 key 所代表的值，并转换成 int64。
+// 若不存在或是转换出错，则返回 def 作为其默认值。
+// 仅在类型转换出错时，才会向 errors 写入错误信息。
+func (p *Params) MustInt64(key string, def int64) int64 {
+	str, found := p.params[key]
+
+	// 不存在，仅返回默认值，不算错误
+	if !found {
+		return def
+	}
+
+	ret, err := strconv.ParseInt(str, 10, 64)
+	if err != nil {
+		p.errors[key] = err.Error()
+		return def
+	}
+
+	return ret
+}
+
+// String 获取参数 key 所代表的值，并转换成 string。
+func (p *Params) String(key string) string {
+	ret, err := p.params.String(key)
+	if err != nil {
+		p.errors[key] = err.Error()
+	}
+
+	return ret
+}
+
+// MustString 获取参数 key 所代表的值，并转换成 string。
+// 若不存在或是转换出错，则返回 def 作为其默认值。
+func (p *Params) MustString(key, def string) string {
+	ret, found := p.params[key]
+
+	// 不存在，仅返回默认值，不算错误
+	if !found {
+		return def
+	}
+	return ret
+}
+
+// Bool 获取参数 key 所代表的值，并转换成 bool。
+//
+// 最终会调用 strconv.ParseBool 进行转换，
+// 也只有该方法中允许的字符串会被正确转换。
+func (p *Params) Bool(key string) bool {
+	ret, err := p.params.Bool(key)
+	if err != nil {
+		p.errors[key] = err.Error()
+	}
+
+	return ret
+}
+
+// MustBool 获取参数 key 所代表的值，并转换成 bool。
+// 若不存在或是转换出错，则返回 def 作为其默认值。
+// 仅在类型转换出错时，才会向 errors 写入错误信息。
+//
+// 最终会调用 strconv.ParseBool 进行转换，
+// 也只有该方法中允许的字符串会被正确转换。
+func (p *Params) MustBool(key string, def bool) bool {
+	str, found := p.params[key]
+
+	// 不存在，仅返回默认值，不算错误
+	if !found {
+		return def
+	}
+
+	ret, err := strconv.ParseBool(str)
+	if err != nil {
+		p.errors[key] = err.Error()
+		return def
+	}
+
+	return ret
+}
+
+// Float64 获取参数 key 所代表的值，并转换成 float64。
+func (p *Params) Float64(key string) float64 {
+	ret, err := p.params.Float(key)
+	if err != nil {
+		p.errors[key] = err.Error()
+	}
+
+	return ret
+}
+
+// MustFloat64 获取参数 key 所代表的值，并转换成 float64。
+// 若不存在或是转换出错，则返回 def 作为其默认值。
+// 仅在类型转换出错时，才会向 errors 写入错误信息。
+func (p *Params) MustFloat64(key string, def float64) float64 {
+	str, found := p.params[key]
+
+	// 不存在，仅返回默认值，不算错误
+	if !found {
+		return def
+	}
+
+	ret, err := strconv.ParseFloat(str, 64)
+	if err != nil {
+		p.errors[key] = err.Error()
+		return def
+	}
+
+	return ret
+}
+
+// HasErrors 是否有错误内容存在
+func (p *Params) HasErrors() bool {
+	return len(p.errors) > 0
+}
+
+// Errors 返回所有的错误信息
+func (p *Params) Errors() map[string]string {
+	return p.errors
+}
+
+// ParamID 获取地址参数中表示 ID 的值。相对于 ParamInt64，该值必须大于 0。
+//
+// NOTE: 若需要获取其它类型的数据，可以使用 Context.Params 来获取。
+// Context.ParamInt64 和 Context.ParamID 仅作为一个简便的操作存在。
+func (ctx *Context) ParamID(key string) (int64, error) {
+	id, err := ctx.ParamInt64(key)
+
+	if err != nil {
+		return 0, err
+	}
+
+	if id <= 0 {
+		return 0, errors.New("必须大于 0")
+	}
+
+	return id, nil
+}
+
+// ParamInt64 取地址参数中的 int64 值。
+//
+// NOTE: 若需要获取其它类型的数据，可以使用 Context.Params 来获取。
+// Context.ParamInt64 和 Context.ParamID 仅作为一个简便的操作存在。
+func (ctx *Context) ParamInt64(key string) (int64, error) {
+	p := ctx.Params()
+
+	ret, err := p.params.Int(key)
+	if err != nil {
+		return 0, err
+	}
+
+	return ret, nil
+}
---
+++ context/params_test.go
@@ -0,0 +1,243 @@
+// Copyright 2016 by caixw, All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+package context
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/issue9/assert"
+	"github.com/issue9/mux"
+)
+
+func newContextWithParam(w http.ResponseWriter, r *http.Request, a *assert.Assertion) *Context {
+	r.Header.Set("Accept", "*/*")
+	ctx, err := New(w, r, DefaultEncoding, DefaultCharset, true)
+	a.NotError(err).NotNil(ctx)
+
+	return ctx
+}
+
+func TestParams_empty(t *testing.T) {
+	a := assert.New(t)
+	mux := mux.New(true, true, nil, nil)
+	srv := httptest.NewServer(mux)
+	defer srv.Close()
+
+	mux.GetFunc("/params/empty", func(w http.ResponseWriter, r *http.Request) {
+		ctx := newContextWithParam(w, r, a)
+		ps := ctx.Params()
+
+		a.Equal(ps.Int("id1"), 0)
+		a.Equal(ps.MustInt("id2", 2), 2)
+		a.True(ps.HasErrors()).
+			Equal(1, len(ps.Errors())) // MustInt 在不存在的情况，并不会生成错误信息
+	})
+
+	resp, err := http.Get(srv.URL + "/params/empty")
+	a.NotError(err).NotNil(resp)
+}
+
+func TestParams_ID_MustID(t *testing.T) {
+	a := assert.New(t)
+	mux := mux.New(true, true, nil, nil)
+	srv := httptest.NewServer(mux)
+	defer srv.Close()
+
+	mux.GetFunc("/params/id/{i1:\\d+}/{i2}/{str}", func(w http.ResponseWriter, r *http.Request) {
+		ctx := newContextWithParam(w, r, a)
+		ps := ctx.Params()
+
+		a.Equal(ps.ID("i1"), 1)
+
+		// 负数
+		a.Equal(ps.ID("i2"), -2)
+		a.Equal(len(ps.errors), 1)
+
+		// 不存在的参数，添加错误信息
+		a.Equal(ps.ID("i3"), 0)
+		a.Equal(len(ps.errors), 2)
+
+		// MustID() 不会增加错误信息
+		a.Equal(ps.MustID("i3", 3), 3)
+		a.Equal(len(ps.errors), 2)
+
+		// MustID() 负数
+		a.Equal(ps.MustID("i2", 3), 3)
+		a.Equal(len(ps.errors), 2) // 之前已经有一个名为 i2 的错误信息，所以此处为覆盖
+
+		// MustID() 无法转换，会返回默认值，且添加错误信息
+		a.Equal(ps.MustID("str", 3), 3)
+		a.Equal(len(ps.Errors()), 3)
+	})
+
+	resp, err := http.Get(srv.URL + "/params/id/1/-2/str")
+	a.NotError(err).NotNil(resp)
+}
+
+func TestParams_Int_MustInt(t *testing.T) {
+	a := assert.New(t)
+	mux := mux.New(true, true, nil, nil)
+	srv := httptest.NewServer(mux)
+	defer srv.Close()
+
+	mux.GetFunc("/params/int/{i1:\\d+}/{i2:\\d+}/{str}", func(w http.ResponseWriter, r *http.Request) {
+		ctx := newContextWithParam(w, r, a)
+		ps := ctx.Params()
+
+		a.Equal(ps.Int("i1"), 1)
+		a.Equal(ps.Int("i2"), 2)
+
+		// 不存在的参数，添加错误信息
+		a.Equal(ps.Int("i3"), 0)
+		a.Equal(len(ps.errors), 1)
+
+		// MustInt() 不会增加错误信息
+		a.Equal(ps.MustInt("i3", 3), 3)
+		a.Equal(len(ps.errors), 1)
+
+		// MustInt() 无法转换，会返回默认值，且添加错误信息
+		a.Equal(ps.MustInt("str", 3), 3)
+		a.Equal(len(ps.errors), 2)
+	})
+
+	resp, err := http.Get(srv.URL + "/params/int/1/2/str")
+	a.NotError(err).NotNil(resp)
+}
+
+func TestParams_Bool_MustBool(t *testing.T) {
+	a := assert.New(t)
+	mux := mux.New(true, true, nil, nil)
+	srv := httptest.NewServer(mux)
+	defer srv.Close()
+
+	mux.GetFunc("/params/bool/{b1}/{b2}/{str}", func(w http.ResponseWriter, r *http.Request) {
+		ctx := newContextWithParam(w, r, a)
+		ps := ctx.Params()
+
+		a.True(ps.Bool("b1"))
+		a.False(ps.Bool("b2"))
+
+		// 不存在的参数，添加错误信息
+		a.False(ps.Bool("b3"))
+		a.Equal(len(ps.errors), 1)
+
+		// MustBool() 不会增加错误信息
+		a.True(ps.MustBool("b3", true))
+		a.Equal(len(ps.errors), 1)
+
+		// MustBool() 无法转换，会返回默认值，且添加错误信息
+		a.True(ps.MustBool("str", true), true)
+		a.Equal(len(ps.Errors()), 2)
+	})
+
+	resp, err := http.Get(srv.URL + "/params/bool/true/false/str")
+	a.NotError(err).NotNil(resp)
+}
+
+func TestParams_String_MustString(t *testing.T) {
+	a := assert.New(t)
+	mux := mux.New(true, true, nil, nil)
+	srv := httptest.NewServer(mux)
+	defer srv.Close()
+
+	mux.GetFunc("/params/string/{s1}/{s2}", func(w http.ResponseWriter, r *http.Request) {
+		ctx := newContextWithParam(w, r, a)
+		ps := ctx.Params()
+
+		a.Equal(ps.String("s1"), "str1")
+		a.Equal(ps.String("s2"), "str2")
+		a.False(ps.HasErrors())
+
+		// 不存在的参数，添加错误信息
+		a.Equal(ps.String("s3"), "")
+		a.Equal(len(ps.errors), 1)
+
+		// MustString() 不会增加错误信息
+		a.Equal(ps.MustString("s3", "str3"), "str3")
+		a.Equal(len(ps.Errors()), 1)
+	})
+
+	resp, err := http.Get(srv.URL + "/params/string/str1/str2")
+	a.NotError(err).NotNil(resp)
+}
+
+func TestParams_Float_MustFloat(t *testing.T) {
+	a := assert.New(t)
+	mux := mux.New(true, true, nil, nil)
+	srv := httptest.NewServer(mux)
+	defer srv.Close()
+
+	mux.GetFunc("/params/float/{f1}/{f2}/{str}", func(w http.ResponseWriter, r *http.Request) {
+		ctx := newContextWithParam(w, r, a)
+		ps := ctx.Params()
+
+		a.Equal(ps.Float64("f1"), 1.1)
+		a.Equal(ps.Float64("f2"), 2.2)
+		a.False(ps.HasErrors())
+
+		// 不存在的参数，添加错误信息
+		a.Equal(ps.Float64("f3"), 0.0)
+		a.Equal(len(ps.errors), 1)
+
+		// MustFloat64() 不会增加错误信息
+		a.Equal(ps.MustFloat64("id3", 3.3), 3.3)
+		a.Equal(len(ps.errors), 1)
+
+		// MustFloat64() 无法转换，会返回默认值，且添加错误信息
+		a.Equal(ps.MustFloat64("str", 3.3), 3.3)
+		a.Equal(len(ps.Errors()), 2)
+	})
+
+	resp, err := http.Get(srv.URL + "/params/float/1.1/2.2/str")
+	a.NotError(err).NotNil(resp)
+}
+
+func TestParams_ParamID(t *testing.T) {
+	a := assert.New(t)
+	mux := mux.New(true, true, nil, nil)
+	srv := httptest.NewServer(mux)
+	defer srv.Close()
+
+	mux.GetFunc("/params/paramid/{i1}/{i2}/{str}", func(w http.ResponseWriter, r *http.Request) {
+		ctx := newContextWithParam(w, r, a)
+
+		i1, err := ctx.ParamID("i1")
+		a.NotError(err).Equal(i1, 1)
+
+		i2, err := ctx.ParamID("i2")
+		a.Error(err).Equal(i2, 0)
+
+		i3, err := ctx.ParamID("i3")
+		a.Error(err).Equal(i3, 0)
+	})
+
+	resp, err := http.Get(srv.URL + "/params/paramid/1/-2/str")
+	a.NotError(err).NotNil(resp)
+}
+
+func TestParams_ParamInt64(t *testing.T) {
+	a := assert.New(t)
+	mux := mux.New(true, true, nil, nil)
+	srv := httptest.NewServer(mux)
+	defer srv.Close()
+
+	mux.GetFunc("/params/paramint64/{i1}/{i2}/{str}", func(w http.ResponseWriter, r *http.Request) {
+		ctx := newContextWithParam(w, r, a)
+
+		i1, err := ctx.ParamInt64("i1")
+		a.NotError(err).Equal(i1, 1)
+
+		i2, err := ctx.ParamInt64("i2")
+		a.NotError(err).Equal(i2, -2)
+
+		i3, err := ctx.ParamInt64("i3")
+		a.Error(err).Equal(i3, 0)
+	})
+
+	resp, err := http.Get(srv.URL + "/params/paramint64/1/-2/str")
+	a.NotError(err).NotNil(resp)
+}
