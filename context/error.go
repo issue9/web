33a66de740d76d21722bf9aa14a44b9d55bf4a63
caixw <xnotepad@gmail.com>
2018-03-31 00:08:21 +0800

    添加 context.Panic 方法

--- context/error.go
+++ context/error.go
@@ -29,6 +29,14 @@ func (ctx *Context) Error(status int, v ...interface{}) {
 	ctx.RenderStatus(status)
 }
 
+// Panic 以指定的状态码抛出异常
+//
+// 与 Error 的不同在于：
+// Error 不会主动退出当前协程，而 Panic 则会触发 panic，退出当前协程。
+func (ctx *Context) Panic(status int) {
+	Panic(status)
+}
+
 // Critical 输出一条日志到 CRITICAL 日志通道，
 // 并向用户输出一个指定状态码的页面。
 // 若是输出日志的过程中出错，则 panic
