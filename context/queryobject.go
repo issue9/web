36d0c65aa6959262f301ea8497493675bc0c5273
caixw <xnotepad@gmail.com>
2018-05-01 20:15:50 +0800

    改进文档

--- context/queryobject.go
+++ context/queryobject.go
@@ -14,13 +14,29 @@ import (
 
 const queryTag = "query"
 
-// QuerySanitizer 验证接口
+// QuerySanitizer 表示对一个查询参数构成的结构体进行数据验证和内容修正的接口
 type QuerySanitizer interface {
-	QuerySanitize(map[string]string)
+	// 参数 errors 用来保存由函数中发现的错误信息。
+	//
+	// 其中的键名为错误字段名称，键值为错误信息。
+	QuerySanitize(errors map[string]string)
 }
 
-// UnmarshalQueryer 将一个值转换成 Query 对象中的值
+// UnmarshalQueryer 该接口实现在了将一些特定的查询参数格式转换成其它类型的接口。
+//
+// 比如一个查询参数格式如下：
+//  /path?state=locked
+// 而实际上后端的将 state 表示为一个数值：
+//  type State int8
+//  const StateLocked State = 1
+// 那么只要 State 实现 UnmarshalQueryer 接口，就可以实现将 locked 转换成 1 的能力。
+//  func (s *State) UnmarshalQuery(data string) error {
+//      if data == "locked" {
+//          *s = StateLocked
+//      }
+//  }
 type UnmarshalQueryer interface {
+	// data 表示由查询参数传递过来的单个值。
 	UnmarshalQuery(data string) error
 }
 
@@ -28,7 +44,34 @@ type UnmarshalQueryer interface {
 //
 // 返回的是每一个字段对应的错误信息。
 //
-//  `query:"name,default-value"`
+//
+// struct tag
+//
+// 通过 struct tag 的方式将查询参数与结构体中的字段进行关联。
+// struct tag 的格式如下：
+//  `query:"name,default"`
+// 其中 name 为对应查询参数的名称，或是为空则采用字段本身的名称；
+// default 表示在没有参数的情况下，采用的默认值，可以为空。
+//
+//
+// 数组：
+//
+// 如果字段表示的是切片，那么查询参数的值，将以半角逗号作为分隔符进行转换写入到切片中。
+// struct tag 中的默认值，也可以指定多个：
+//  type Object struct {
+//      Slice []int `query:"name,1,2"`
+//  }
+// 以上内容，在没有指定参数的情况下，Slice 会被指定为 []int{1,2}
+//
+//
+// 默认值：
+//
+// 默认值可以通过 struct tag 指定，也可以通过在初始化对象时，另外指定：
+//  obj := &Object{
+//      Slice: []int{3,4,5}
+//  }
+// 以上内容，在不传递参数时，会采用 []int{3,4,5} 作为其默认值，而不是 struct tag
+// 中指定的 []int{1,2}。
 func (ctx *Context) QueryObject(v interface{}) (errors map[string]string) {
 	rval := reflect.ValueOf(v)
 	for rval.Kind() == reflect.Ptr {
