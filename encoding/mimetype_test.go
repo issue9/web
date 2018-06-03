c268dfc867d55ea1a860edbba9e01d5e7815e198
caixw <xnotepad@gmail.com>
2018-06-04 00:24:10 +0800

    添加测试内容

--- encoding/mimetype_test.go
+++ encoding/mimetype_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/issue9/assert"
 )
 
-func TestMarshal(t *testing.T) {
+func testMarshal(t *testing.T) {
 	a := assert.New(t)
 
 	a.Nil(Marshal("not exists"))
@@ -24,7 +24,7 @@ func TestMarshal(t *testing.T) {
 	a.NotNil(Marshal("json"))
 }
 
-func TestUnmarshal(t *testing.T) {
+func testUnmarshal(t *testing.T) {
 	a := assert.New(t)
 
 	a.Nil(Unmarshal("not exists"))
@@ -35,4 +35,74 @@ func TestUnmarshal(t *testing.T) {
 
 	a.NotError(AddUnmarshal("json", json.Unmarshal))
 	a.NotNil(Unmarshal("json"))
+}
+
+func TestMimeType(t *testing.T) {
+	a := assert.New(t)
+
+	m, um := MimeType(DefaultMimeType)
+	a.NotNil(m).NotNil(um)
+
+	// 并未指定 text/*
+	m, um = MimeType("text")
+	a.Nil(m).Nil(um)
+
+	// 未指定 font
+	m, um = MimeType("font")
+	a.Nil(m).Nil(um)
+
+	a.NotError(AddMimeType("text/*", TextMarshal, TextUnmarshal))
+	m, um = MimeType("text")
+	a.NotNil(m).NotNil(um)
+	m, um = MimeType("text/html") // 不存在
+	a.Nil(m).Nil(um)
+
+	a.NotError(AddMimeType("text/html", TextMarshal, TextUnmarshal))
+	m, um = MimeType("text/html")
+	a.NotNil(m).NotNil(um)
+
+	// 根元素未安装
+	m, um = MimeType("*/*")
+	a.Nil(m).Nil(um)
+
+	a.NotError(AddMimeType("*/*", TextMarshal, TextUnmarshal))
+	m, um = MimeType("*")
+	a.NotNil(m).NotNil(um)
+
+	a.Error(AddMimeType("", TextMarshal, TextUnmarshal))
+}
+
+func TestMimetype_set(t *testing.T) {
+	a := assert.New(t)
+
+	mt := &mimetype{}
+	a.NotError(mt.set(TextMarshal, TextUnmarshal))
+
+	a.ErrorType(mt.set(TextMarshal, nil), ErrExists)
+	a.ErrorType(mt.set(nil, TextUnmarshal), ErrExists)
+}
+
+func TestParseMimeType(t *testing.T) {
+	a := assert.New(t)
+
+	name, subname := parseMimeType("*/*")
+	a.Equal(name, "*").Equal(subname, "*")
+
+	name, subname = parseMimeType("*/")
+	a.Equal(name, "*").Equal(subname, "")
+
+	name, subname = parseMimeType("*/text")
+	a.Equal(name, "*").Equal(subname, "text")
+
+	name, subname = parseMimeType("text/*")
+	a.Equal(name, "text").Equal(subname, "*")
+
+	name, subname = parseMimeType("text/")
+	a.Equal(name, "text").Equal(subname, "")
+
+	name, subname = parseMimeType("/")
+	a.Equal(name, "/").Equal(subname, "")
+
+	name, subname = parseMimeType("/*")
+	a.Equal(name, "/*").Equal(subname, "")
 }
