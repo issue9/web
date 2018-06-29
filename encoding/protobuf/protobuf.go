e8de2cbc899db52820562d9075448663c678d9b7
caixw <xnotepad@gmail.com>
2018-06-30 01:19:04 +0800

    添加对 google protocol buffers 协议的支持
    直接引用 github.com/golang/protobuf 包，仅作一个简单的封装

---
+++ encoding/protobuf/protobuf.go
@@ -0,0 +1,37 @@
+// Copyright 2018 by caixw, All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+// Package protobuf 提供对 Google protocol buffers 的支持
+package protobuf
+
+import (
+	"errors"
+
+	"github.com/golang/protobuf/proto"
+)
+
+// MimeType 当前协议的 mimetype 值
+const MimeType = "application/octet-stream"
+
+var errInvalidType = errors.New("无效的类型，只能是 protof.Message")
+
+// Marshal 提供对 protobuf 的支持
+func Marshal(v interface{}) ([]byte, error) {
+	p, ok := v.(proto.Message)
+	if !ok {
+		return nil, errInvalidType
+	}
+
+	return proto.Marshal(p)
+}
+
+// Unmarshal 提供对 protobuf 的支持
+func Unmarshal(buf []byte, v interface{}) error {
+	p, ok := v.(proto.Message)
+	if !ok {
+		return errInvalidType
+	}
+
+	return proto.Unmarshal(buf, p)
+}
