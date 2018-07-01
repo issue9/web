7a25e2ed1f76b4e12c23e2823dc088623ca3e1eb
caixw <xnotepad@gmail.com>
2018-07-01 16:45:14 +0800

    修正测试错误

--- module/install_test.go
+++ module/install_test.go
@@ -16,15 +16,12 @@ func TestModule_GetInstall(t *testing.T) {
 
 	m1 := New(router, "users1", "users1 module", "users2")
 	m1.Task("v1", "安装数据表users", func() error { return errors.New("默认用户为admin:123") })
+
 	f := m1.GetInstall("v1")
 	a.NotNil(f)
-	a.Nil(m1.GetInstall("not-exists"))
+	a.NotError(f())
 
-	m3 := New(router, "users2", "users2 mdoule")
-	m3.Task("v1", "安装数据表users", func() error { return nil })
-	m3.Task("v1", "安装数据表users", func() error { return errors.New("falid message") })
-	m3.Task("v1", "安装数据表users", func() error { return nil })
-
-	a.NotNil(m1.GetInstall("v1"))
-	a.Nil(m1.GetInstall("not-exists"))
+	f = m1.GetInstall("not-exists")
+	a.NotNil(f)
+	a.NotError(f())
 }
