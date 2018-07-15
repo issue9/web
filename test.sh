ea1a49451979867be500c868fc0532a7b8938224
caixw <xnotepad@gmail.com>
2018-07-15 12:11:52 +0800

    添加 test.sh 简化测试时的操作流程

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Copyright 2018 by caixw, All rights reserved.
+# Use of this source code is governed by a MIT
+# license that can be found in the LICENSE file.
+
+echo '生成 so 文件'
+go generate ./...
+
+echo '执行 go test'
+go test ./...
+
+echo '执行 go vet'
+go vet ./...
