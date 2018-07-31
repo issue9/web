e2eb3b981a72d91aae0c752a83fd74c77fc182a3
caixw <xnotepad@gmail.com>
2018-07-31 21:20:20 +0800

    调整 test.sh 代码
    添加 -c 和 -v 两个参数，用于区别 CI 和 手动调用 test.sh：
    手动调用，一般不需要代码覆盖率文件和详细的执行信息

--- test.sh
+++ test.sh
@@ -3,13 +3,28 @@
 # Use of this source code is governed by a MIT
 # license that can be found in the LICENSE file.
 
+# 可以传递两个参数：
+# -c 表示需要输出代码测试覆盖率到文件 coverage.txt 中；
+# -v 表示需要输出详细的执行信息到终端。
+
 list=$(go list ./...| grep -v /vendor/)
 
+while getopts 'vc' OPT; do
+    case $OPT in
+        v)
+            v=-v;;
+        c)
+            c='-coverprofile=coverage.txt -covermode=atomic';;
+        ?)
+            echo '未知的参数'
+    esac
+done
+
 echo '生成 so 文件'
-go generate -v $list
+go generate $v $list
 
 echo '执行 go vet'
-go vet  -v $list
+go vet $v $list
 
 echo '执行 go test'
-go test -v $list
+go test $v $c $list
