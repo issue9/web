d62f8acf9d2933f993b0370699c7294261cf542a
caixw <xnotepad@gmail.com>
2018-07-31 20:47:41 +0800

    CI 调用 test.sh 完成工作

--- test.sh
+++ test.sh
@@ -3,11 +3,13 @@
 # Use of this source code is governed by a MIT
 # license that can be found in the LICENSE file.
 
+list=$(go list ./...| grep -v /vendor/)
+
 echo '生成 so 文件'
-go generate ./...
+go generate -v $list
 
 echo '执行 go vet'
-go vet ./...
+go vet  -v $list
 
 echo '执行 go test'
-go test ./...
+go test -v $list
