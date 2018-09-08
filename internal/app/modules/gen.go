7bd8cade9b817274d5c668bb039df70d38228b0a
caixw <xnotepad@gmail.com>
2018-09-08 10:25:20 +0800

    将 go:generate 指令从 plugin_test.go 分离出来。
    update #3

---
+++ internal/app/modules/gen.go
@@ -0,0 +1,13 @@
+// Copyright 2018 by caixw, All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+// +build linux,drawin
+
+//go:generate go build -o=./testdata/plugin_1.so -buildmode=plugin ./testdata/plugin1/plugin.go
+//go:generate go build -o=./testdata/plugin_2.so -buildmode=plugin ./testdata/plugin2/plugin.go
+//go:generate go build -o=./testdata/plugin-3.so -buildmode=plugin ./testdata/plugin3/plugin.go
+
+package modules
+
+// 此文件仅作为生成测试 *.so 文件，需要注意保持上面的 +build 指令中的值与 pluginOS 中的变量一致。
