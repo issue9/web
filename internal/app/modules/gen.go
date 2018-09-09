858846104ca82595e0fff4c79a7b64ad1aedfe35
caixw <xnotepad@gmail.com>
2018-09-09 10:39:31 +0800

    修正编译条件指令错误

--- internal/app/modules/gen.go
+++ internal/app/modules/gen.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a MIT
 // license that can be found in the LICENSE file.
 
-// +build linux,drawin
+// +build linux darwin
 
 //go:generate go build -o=./testdata/plugin_1.so -buildmode=plugin ./testdata/plugin1/plugin.go
 //go:generate go build -o=./testdata/plugin_2.so -buildmode=plugin ./testdata/plugin2/plugin.go
@@ -10,4 +10,5 @@
 
 package modules
 
-// 此文件仅作为生成测试 *.so 文件，需要注意保持上面的 +build 指令中的值与 pluginOS 中的变量一致。
+// 此文件仅作为生成测试 *.so 文件，
+// 需要注意保持上面的 +build 指令中的值与 pluginOS 变量中的一致。
