63d242642205838f31cad7161ec297cc3abda08a
caixw <xnotepad@gmail.com>
2018-11-29 01:10:32 +0800

    添加 mimetypes 对象

---
+++ mimetype/mimetypes.go
@@ -0,0 +1,208 @@
+// Copyright 2018 by caixw, All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+package mimetype
+
+import (
+	"errors"
+	"sort"
+	"strings"
+
+	"github.com/issue9/middleware/compress/accept"
+)
+
+var (
+	// ErrExists 表示指定名称的项目已经存在。
+	//
+	// 在 AddCharset、Addmarshal 和 AddUnmarshal 中会返回此错误。
+	ErrExists = errors.New("该名称的项目已经存在")
+
+	// ErrInvalidMimetype 无效的 mimetype 值，一般为 content-type 或
+	// Accept 等报头指定的 mimetype 值无效。
+	ErrInvalidMimetype = errors.New("mimetype 无效")
+)
+
+// Mimetypes 管理 mimetype 的处理函数
+type Mimetypes struct {
+	marshals   []*marshaler
+	unmarshals []*unmarshaler
+}
+
+type marshaler struct {
+	f    MarshalFunc
+	name string
+}
+
+type unmarshaler struct {
+	f    UnmarshalFunc
+	name string
+}
+
+// New 声明新的 Mimetypes 实例
+func New() *Mimetypes {
+	return &Mimetypes{
+		marshals:   make([]*marshaler, 0, 10),
+		unmarshals: make([]*unmarshaler, 0, 10),
+	}
+}
+
+// Unmarshal 查找指定名称的 UnmarshalFunc
+func (m *Mimetypes) Unmarshal(name string) (UnmarshalFunc, error) {
+	var unmarshal *unmarshaler
+	for _, mt := range m.unmarshals {
+		if mt.name == name {
+			unmarshal = mt
+			break
+		}
+	}
+	if unmarshal == nil {
+		return nil, ErrInvalidMimetype
+	}
+
+	return unmarshal.f, nil
+}
+
+// Marshal 从 header 解析出当前请求所需要的解 mimetype 名称和对应的解码函数
+//
+// */* 或是空值 表示匹配任意内容，一般会选择第一个元素作匹配；
+// xx/* 表示匹配以 xx/ 开头的任意元素，一般会选择 xx/* 开头的第一个元素；
+// xx/ 表示完全匹配以 xx/ 的内容
+// 如果传递的内容如下：
+//  application/json;q=0.9,*/*;q=1
+// 则因为 */* 的 q 值比较高，而返回 */* 匹配的内容
+//
+// 在不完全匹配的情况下，返回值的名称依然是具体名称。
+//  text/*;q=0.9
+// 返回的名称可能是：
+//  text/plain
+func (m *Mimetypes) Marshal(header string) (string, MarshalFunc, error) {
+	if header == "" {
+		if mm := m.findMarshal("*/*"); mm != nil {
+			return mm.name, mm.f, nil
+		}
+		return "", nil, ErrInvalidMimetype
+	}
+
+	accepts, err := accept.Parse(header)
+	if err != nil {
+		return "", nil, err
+	}
+
+	for _, accept := range accepts {
+		if mm := m.findMarshal(accept.Value); mm != nil {
+			return mm.name, mm.f, nil
+		}
+	}
+
+	return "", nil, ErrInvalidMimetype
+}
+
+// AddMarshals 添加多个编码函数
+func (m *Mimetypes) AddMarshals(ms map[string]MarshalFunc) error {
+	for k, v := range ms {
+		if err := m.AddMarshal(k, v); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// AddMarshal 添加编码函数
+func (m *Mimetypes) AddMarshal(name string, mf MarshalFunc) error {
+	if strings.HasSuffix(name, "/*") || name == "*" {
+		return ErrInvalidMimetype
+	}
+
+	for _, mt := range m.marshals {
+		if mt.name == name {
+			return ErrExists
+		}
+	}
+
+	m.marshals = append(m.marshals, &marshaler{
+		f:    mf,
+		name: name,
+	})
+
+	sort.SliceStable(m.marshals, func(i, j int) bool {
+		if m.marshals[i].name == DefaultMimetype {
+			return true
+		}
+
+		if m.marshals[j].name == DefaultMimetype {
+			return false
+		}
+
+		return m.marshals[i].name < m.marshals[j].name
+	})
+
+	return nil
+}
+
+// AddUnmarshals 添加多个编码函数
+func (m *Mimetypes) AddUnmarshals(ms map[string]UnmarshalFunc) error {
+	for k, v := range ms {
+		if err := m.AddUnmarshal(k, v); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// AddUnmarshal 添加编码函数
+func (m *Mimetypes) AddUnmarshal(name string, mm UnmarshalFunc) error {
+	if strings.IndexByte(name, '*') >= 0 {
+		return ErrInvalidMimetype
+	}
+
+	for _, mt := range m.unmarshals {
+		if mt.name == name {
+			return ErrExists
+		}
+	}
+
+	m.unmarshals = append(m.unmarshals, &unmarshaler{
+		f:    mm,
+		name: name,
+	})
+
+	sort.SliceStable(m.unmarshals, func(i, j int) bool {
+		if m.unmarshals[i].name == DefaultMimetype {
+			return true
+		}
+
+		if m.unmarshals[j].name == DefaultMimetype {
+			return false
+		}
+
+		return m.unmarshals[i].name < m.unmarshals[j].name
+	})
+
+	return nil
+}
+
+func (m *Mimetypes) findMarshal(name string) *marshaler {
+	switch {
+	case len(m.marshals) == 0:
+		return nil
+	case name == "" || name == "*/*":
+		return m.marshals[0] // 由 len(marshals) == 0 确保最少有一个元素
+	case strings.HasSuffix(name, "/*"):
+		prefix := name[:len(name)-3]
+		for _, mt := range m.marshals {
+			if strings.HasPrefix(mt.name, prefix) {
+				return mt
+			}
+		}
+	default:
+		for _, mt := range m.marshals {
+			if mt.name == name {
+				return mt
+			}
+		}
+	}
+	return nil
+}
---
+++ mimetype/mimetypes_test.go
@@ -0,0 +1,153 @@
+// Copyright 2018 by caixw, All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+package mimetype
+
+import (
+	"testing"
+
+	"github.com/issue9/assert"
+	"github.com/issue9/web/mimetype/gob"
+)
+
+func TestMimetypes_Unmarshal(t *testing.T) {
+	a := assert.New(t)
+
+	m := New()
+	um, err := m.Unmarshal("")
+	a.Error(err).
+		Nil(um)
+
+	a.NotError(m.AddUnmarshal(DefaultMimetype, gob.Unmarshal))
+	a.NotError(m.AddMarshal(DefaultMimetype, gob.Marshal))
+
+	// 未指定 mimetype
+	um, err = m.Unmarshal("")
+	a.Error(err).Nil(um)
+
+	// mimetype 无法找到
+	um, err = m.Unmarshal("not-exists")
+	a.Error(err).Nil(um)
+}
+
+func TestMarshal(t *testing.T) {
+	a := assert.New(t)
+	m := New()
+
+	name, marshal, err := m.Marshal(DefaultMimetype)
+	a.Error(err).
+		Nil(marshal).
+		Empty(name)
+
+	a.NotError(m.AddMarshal(DefaultMimetype, gob.Marshal))
+	a.NotError(m.AddMarshal("text/plain", gob.Marshal))
+
+	name, marshal, err = m.Marshal(DefaultMimetype)
+	a.NotError(err).
+		Equal(marshal, MarshalFunc(gob.Marshal)).
+		Equal(name, DefaultMimetype)
+
+	name, marshal, err = m.Marshal(DefaultMimetype)
+	a.NotError(err).
+		Equal(marshal, MarshalFunc(gob.Marshal)).
+		Equal(name, DefaultMimetype)
+
+	// */* 如果指定了 DefaultMimetype，则必定是该值
+	name, marshal, err = m.Marshal("*/*")
+	a.NotError(err).
+		Equal(marshal, MarshalFunc(gob.Marshal)).
+		Equal(name, DefaultMimetype)
+
+	// 同 */*
+	name, marshal, err = m.Marshal("")
+	a.NotError(err).
+		Equal(marshal, MarshalFunc(gob.Marshal)).
+		Equal(name, DefaultMimetype)
+
+	name, marshal, err = m.Marshal("*/*,text/plain")
+	a.NotError(err).
+		Equal(marshal, MarshalFunc(gob.Marshal)).
+		Equal(name, "text/plain")
+
+	name, marshal, err = m.Marshal("font/wotff;q=x.9")
+	a.Error(err).
+		Empty(name).
+		Nil(marshal)
+
+	name, marshal, err = m.Marshal("font/wotff")
+	a.Error(err).
+		Empty(name).
+		Nil(marshal)
+}
+
+func TestAddMarshal(t *testing.T) {
+	a := assert.New(t)
+	m := New()
+
+	// 不能添加同名的多次
+	a.NotError(m.AddMarshal(DefaultMimetype, nil))
+	a.ErrorType(m.AddMarshal(DefaultMimetype, nil), ErrExists)
+
+	// 不能添加以 /* 结属的名称
+	a.ErrorType(m.AddMarshal("application/*", nil), ErrExists)
+	a.ErrorType(m.AddMarshal("/*", nil), ErrExists)
+
+	// 排序是否正常
+	a.NotError(m.AddMarshal("application/json", nil))
+	a.Equal(m.marshals[0].name, DefaultMimetype) // 默认始终在第一
+}
+
+func TestAddUnmarshal(t *testing.T) {
+	a := assert.New(t)
+	m := New()
+
+	a.NotError(m.AddUnmarshal(DefaultMimetype, nil))
+	a.ErrorType(m.AddUnmarshal(DefaultMimetype, nil), ErrExists)
+
+	// 不能添加包含 * 字符的名称
+	a.ErrorType(m.AddUnmarshal("application/*", nil), ErrExists)
+	a.ErrorType(m.AddUnmarshal("*", nil), ErrExists)
+
+	// 排序是否正常
+	a.NotError(m.AddUnmarshal("application/json", nil))
+	a.Equal(m.unmarshals[0].name, DefaultMimetype) // 默认始终在第一
+}
+
+func TestFindMarshal(t *testing.T) {
+	a := assert.New(t)
+	m := New()
+
+	a.NotError(m.AddMarshals(map[string]MarshalFunc{
+		"text":           nil,
+		"text/plain":     nil,
+		"text/text":      nil,
+		"application/aa": nil, // aa 排名靠前
+		"application/bb": nil, // aa 排名靠前
+	}))
+
+	mm := m.findMarshal("text")
+	a.Equal(mm.name, "text")
+
+	mm = m.findMarshal("text/*")
+	a.Equal(mm.name, "text")
+
+	mm = m.findMarshal("application/*")
+	a.Equal(mm.name, "application/aa")
+
+	// 第一条数据
+	mm = m.findMarshal("*/*")
+	a.Equal(mm.name, "application/aa")
+
+	// 第一条数据
+	mm = m.findMarshal("")
+	a.Equal(mm.name, "application/aa")
+
+	// 有默认值，则始终在第一
+	a.NotError(m.AddMarshal(DefaultMimetype, nil))
+	mm = m.findMarshal("*/*")
+	a.Equal(mm.name, DefaultMimetype)
+
+	// 不存在
+	a.Nil(m.findMarshal("xx/*"))
+}
