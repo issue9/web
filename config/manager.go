65a00a40a41be975dd1d265c4f3176573c2836ea
caixw <xnotepad@gmail.com>
2018-11-29 23:13:47 +0800

    添加 manager 实例

---
+++ config/manager.go
@@ -0,0 +1,107 @@
+// Copyright 2018 by caixw, All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+package config
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var errExtNotAllowEmpty = errors.New("扩展名不能为空")
+
+// Sanitizer 如果对象实现了该方法，那么在解析完之后，
+// 会调用该接口的函数对数据进行修正和检测。
+type Sanitizer interface {
+	Sanitize() error
+}
+
+// Manager 配置管理内容
+type Manager struct {
+	dir        string
+	unmarshals map[string]UnmarshalFunc
+}
+
+// NewManager 新的 Manager 实例
+func NewManager(dir string) *Manager {
+	return &Manager{
+		dir:        dir,
+		unmarshals: map[string]UnmarshalFunc{},
+	}
+}
+
+// AddUnmarshal 注册解析函数
+func (mgr *Manager) AddUnmarshal(m UnmarshalFunc, ext ...string) error {
+	for _, e := range ext {
+		if e == "" || e == "." {
+			return errExtNotAllowEmpty
+		}
+
+		if e[0] != '.' {
+			e = "." + e
+		}
+
+		e = strings.ToLower(e)
+		if _, found := mgr.unmarshals[e]; found {
+			return fmt.Errorf("已经存在该扩展名 %s 对应的解析函数", ext)
+		}
+		mgr.unmarshals[e] = m
+	}
+
+	return nil
+}
+
+// SetUnmarshal 修改指定扩展名关联的解析函数，不存在则添加。
+func (mgr *Manager) SetUnmarshal(m UnmarshalFunc, ext ...string) error {
+	for _, e := range ext {
+		if e == "" || e == "." {
+			return errExtNotAllowEmpty
+		}
+
+		if e[0] != '.' {
+			e = "." + e
+		}
+
+		mgr.unmarshals[strings.ToLower(e)] = m
+	}
+
+	return nil
+}
+
+// LoadFile 加载指定的配置文件内容到 v 中
+func (mgr *Manager) LoadFile(path string, v interface{}) error {
+	r, err := os.Open(filepath.Join(mgr.dir, path))
+	if err != nil {
+		return err
+	}
+	return mgr.Load(r, filepath.Ext(path), v)
+}
+
+// Load 加载指定的配置文件内容到 v 中
+func (mgr *Manager) Load(r io.Reader, typ string, v interface{}) error {
+	typ = strings.ToLower(typ)
+	unmarshal, found := mgr.unmarshals[typ]
+	if !found {
+		return fmt.Errorf("无效的配置文件类型：%s", typ)
+	}
+
+	data, err := ioutil.ReadAll(r)
+	if err != nil {
+		return err
+	}
+
+	if err = unmarshal(data, v); err != nil {
+		return err
+	}
+
+	if s, ok := v.(Sanitizer); ok {
+		return s.Sanitize()
+	}
+	return nil
+}
