d892dd15dc2a1c95e84fadaebba272f9aa239766
caixw <xnotepad@gmail.com>
2018-12-05 22:37:37 +0800

    修正文档错误

--- app/errors.go
+++ app/errors.go
@@ -59,15 +59,15 @@ func (app *App) RenderError(w http.ResponseWriter, status int) {
 // 表示一个 HTTP 状态码错误。
 // panic 此类型的值，可以在 Recovery 中作特殊处理。
 //
-// 目前仅由 Exit 使用，让框加以特定的状态码退出当前协程。
+// 目前仅由 ExitContext 使用，让框加以特定的状态码退出当前协程。
 type httpStatus int
 
 // ExitContext 以指定的状态码退出当前协程
 //
 // status 表示输出的状态码，如果为 0，则不会作任何状态码输出。
 //
-// Exit 最终是以 panic 的形式退出，所以如果你的代码里截获了 panic，
-// 那么 Exit 并不能达到退出当前请求的操作。
+// ExitContext 最终是以 panic 的形式退出，所以如果你的代码里截获了 panic，
+// 那么 ExitContext 并不能达到退出当前请求的操作。
 func ExitContext(status int) {
 	panic(httpStatus(status))
 }
