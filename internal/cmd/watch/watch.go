a8c7d9de6971b57a3d63dc8c7f41efceadd4e1bc
caixw <xnotepad@gmail.com>
2018-12-12 21:05:08 +0800

    cmd 添加热编译功能

--- internal/cmd/watch/watch.go
+++ internal/cmd/watch/watch.go
@@ -5,7 +5,11 @@
 package watch
 
 import (
+	"flag"
 	"fmt"
+	"os"
+
+	"github.com/caixw/gobuild"
 
 	"github.com/issue9/web/internal/cmd/help"
 )
@@ -16,7 +20,33 @@ func init() {
 
 // Do 执行子命令
 func Do() error {
-	// TODO
+	var recursive, showIgnore bool
+	var mainFiles, outputName, extString, appArgs string
+
+	set := flag.NewFlagSet("", flag.ExitOnError)
+
+	set.BoolVar(&recursive, "r", true, "是否查找子目录；")
+	set.BoolVar(&showIgnore, "i", false, "是否显示被标记为 IGNORE 的日志内容；")
+	set.StringVar(&outputName, "o", "", "指定输出名称，程序的工作目录随之改变；")
+	set.StringVar(&appArgs, "x", "", "传递给编译程序的参数；")
+	set.StringVar(&extString, "ext", "go", "指定监视的文件扩展，区分大小写。* 表示监视所有类型文件，空值代表不监视任何文件；")
+	set.StringVar(&mainFiles, "main", "", "指定需要编译的文件；")
+	set.Usage = usage
+	set.Parse(os.Args[1:])
+
+	logs := gobuild.NewConsoleLogs(showIgnore)
+
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	dirs := append([]string{wd}, flag.Args()...)
+
+	err = gobuild.Build(logs.Logs, mainFiles, outputName, extString, recursive, appArgs, dirs...)
+	if err != nil {
+		panic(err)
+	}
+	logs.Stop()
 	return nil
 }
 
