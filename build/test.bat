5851d07626eb76f0bc45912d40c8a4e1ea43e436
caixw <xnotepad@gmail.com>
2019-03-07 20:10:12 +0800

    添加注释

--- build/test.bat
+++ build/test.bat
@@ -1,12 +1,12 @@
-# Copyright 2018 by caixw, All rights reserved.
-# Use of this source code is governed by a MIT
-# license that can be found in the LICENSE file.
+:: Copyright 2018 by caixw, All rights reserved.
+:: Use of this source code is governed by a MIT
+:: license that can be found in the LICENSE file.
 
-# 可以传递两个参数：
-# -c 表示需要输出代码测试覆盖率到文件 coverage.txt 中；
-# -v 表示需要输出详细的执行信息到终端。
+:: 可以传递两个参数：
+:: -c 表示需要输出代码测试覆盖率到文件 coverage.txt 中；
+:: -v 表示需要输出详细的执行信息到终端。
 
-SET list = go list %~dp0\..\cmd\web
+SET list = go list %~dp0\..\...
 
 FOR/f %%a IN ("%*") DO (
     IF/I"%%a"=="/v" SET v='-v'
