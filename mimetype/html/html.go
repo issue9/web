717b19d03c77299e065e105809b570f5f8f3715c
caixw <xnotepad@gmail.com>
2019-03-19 20:27:03 +0800

    修正文档错误

--- mimetype/html/html.go
+++ mimetype/html/html.go
@@ -2,11 +2,11 @@
 // Use of this source code is governed by a MIT
 // license that can be found in the LICENSE file.
 
-// Package html 提供输出 HTML 内容的 encoding.MarshalFunc 函数。
+// Package html 提供输出 HTML 内容的 mimetype.MarshalFunc 函数。
 //
 //  tpl := template.ParseFiles(...)
 //  mgr := html.New(tpl)
-//  encoding.AddMarshal("text/html", mgr.Marshal)
+//  mimetype.AddMarshal("text/html", mgr.Marshal)
 //
 //  func handle(w http.ResponseWriter, r *http.Request) {
 //      ctx := web.New(w, r)
@@ -30,9 +30,9 @@ type HTML struct {
 	tpl *template.Template
 }
 
-// Template 传递给 encoding.MarshalFunc 的参数。
+// Template 传递给 mimetype.MarshalFunc 的参数。
 //
-// 因为 encoding.MarshalFunc 限定了只能有一个参数，
+// 因为 mimetype.MarshalFunc 限定了只能有一个参数，
 // 而模板解析，除了传递的值，最起码还要一个模板名称，
 // 所以采用 Template 作了一个简单的包装。
 type Template struct {
