2c0a719bbf85f09d8da610d26b56175bee503b4b
caixw <xnotepad@gmail.com>
2019-05-18 20:03:11 +0800

    [web/cmd] go fmt

--- internal/cmd/create/create.go
+++ internal/cmd/create/create.go
@@ -13,9 +13,9 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/issue9/cmdopt"
 	"github.com/issue9/term/colors"
 	"github.com/issue9/term/prompt"
-	"github.com/issue9/cmdopt"
 	"github.com/issue9/utils"
 	yaml "gopkg.in/yaml.v2"
 
@@ -23,19 +23,19 @@ import (
 )
 
 // Init 初始化函数
-func Init(opt *cmdopt.CmdOpt){
+func Init(opt *cmdopt.CmdOpt) {
 	opt.New("create", do, usage)
 }
 
-func usage(output io.Writer) error{
-	_,err:=fmt.Fprintln(output, `构建一个新的 web 项目
+func usage(output io.Writer) error {
+	_, err := fmt.Fprintln(output, `构建一个新的 web 项目
 
 语法：web create [mod]
 mod 为一个可选参数，如果指定了，则会直接使用此值作为模块名，
 若不指定，则会通过之后的交互要求用户指定。模块名中的最后一
 路径名称，会作为目录名称创建于当前目录下。`)
 
-return err
+	return err
 }
 
 func do(output io.Writer) error {
