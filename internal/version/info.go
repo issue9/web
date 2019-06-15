65f50617e2d68947d5e4baad6fe523cfcc987d31
caixw <xnotepad@gmail.com>
2019-06-15 23:26:33 +0800

    v0.31.0

--- internal/version/info.go
+++ internal/version/info.go
@@ -5,10 +5,13 @@ package version
 // Version 版本号
 //
 // 版本号规则遵循 https://semver.org/lang/zh-CN/
-const Version = "0.29.0"
+const Version = "0.31.0"
 
-// buildDate 编译日期，可以由编译器指定
+// 编译日期，可以由编译器指定
 var buildDate string
+
+// 最后一次提交的 hash 值
+var commitHash string
 
 var fullVersion = Version
 
@@ -19,6 +22,11 @@ func init() {
 }
 
 // FullVersion 完整的版本号，可能包括了编译日期。
+func FullVersion() string {
+	return fullVersion
+}
+
+// CommitHash 最后一次提示我的 hash 值
 func FullVersion() string {
 	return fullVersion
 }
