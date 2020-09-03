8cc760a1a0c5421439a877bf5bb8d73e08aa3b49
caixw <xnotepad@gmail.com>
2020-09-03 23:16:35 +0800

    fix(context): 修正返回错误时取值错误

--- context/context.go
+++ context/context.go
@@ -214,7 +214,7 @@ func (ctx *Context) Marshal(status int, v interface{}, headers map[string]string
 
 	w := transform.NewWriter(ctx.Response, ctx.OutputCharset.NewEncoder())
 	if _, err = w.Write(data); err != nil {
-		if err2 := w.Close(); err != nil {
+		if err2 := w.Close(); err2 != nil {
 			return fmt.Errorf("在处理错误 %w 时再次抛出错误 %s", err, err2.Error())
 		}
 		return err
