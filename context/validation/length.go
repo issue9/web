48674468139866a36fbee625b507c44c705e78a9
caixw <xnotepad@gmail.com>
2020-10-28 19:45:33 +0800

    feat(context/validation): 添加 Range

--- context/validation/length.go
+++ context/validation/length.go
@@ -8,9 +8,13 @@ import (
 
 type lengthRule struct {
 	msg, invalidType string
+	min, max         uint64
+}
 
-	min, max int
-	base     int
+type rangeRule struct {
+	msg, invalidType string
+	min, max         int64
+	valueType        reflect.Type
 }
 
 // Length 声明判断内容长度的验证规则
@@ -19,12 +23,25 @@ type lengthRule struct {
 // invalidType 类型无效时的验证信息；
 //
 // 只能验证类型为 string、Map、Slice 和 Array 的数据。
-func Length(msg, invalidType string, min, max int) Ruler {
+func Length(msg, invalidType string, min, max uint64) Ruler {
 	return &lengthRule{
 		msg:         msg,
 		invalidType: invalidType,
 		min:         min,
 		max:         max,
+	}
+}
+
+// Range 声明判断数值大小的验证规则
+//
+// 只能验证类型为 int、int8、int16、int32、int64、uint、uint8、uint16、uint32、uint64、float32 和 float64 类型的值。
+func Range(msg, invalidType string, min, max int64) Ruler {
+	return &rangeRule{
+		msg:         msg,
+		invalidType: invalidType,
+		min:         min,
+		max:         max,
+		valueType:   reflect.TypeOf(min),
 	}
 }
 
@@ -44,7 +61,26 @@ func (rule *lengthRule) Validate(v interface{}) (msg string) {
 		}
 	}
 
-	if l < rule.min || l > rule.max {
+	if uint64(l) < rule.min || uint64(l) > rule.max {
+		msg = rule.msg
+	}
+	return
+}
+
+func (rule *rangeRule) Validate(v interface{}) (msg string) {
+	var ok bool
+	switch v.(type) {
+	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
+		i := reflect.ValueOf(v).Int()
+		ok = i >= rule.min && i <= rule.max
+	case float32, float64:
+		f := reflect.ValueOf(v).Float()
+		ok = f >= float64(rule.min) && f <= float64(rule.max)
+	default:
+		return rule.invalidType
+	}
+
+	if !ok {
 		msg = rule.msg
 	}
 	return
--- context/validation/length_test.go
+++ context/validation/length_test.go
@@ -3,6 +3,7 @@
 package validation
 
 import (
+	"math"
 	"testing"
 
 	"github.com/issue9/assert"
@@ -16,4 +17,17 @@ func TestLength(t *testing.T) {
 	a.Equal(l.Validate([]byte("123")), "msg")
 	a.Empty(l.Validate([]rune("12345")))
 	a.Equal(l.Validate(&struct{}{}), "invalid-type")
+}
+
+func TestRange(t *testing.T) {
+	a := assert.New(t)
+
+	r := Range("msg", "invali-type", 5, math.MaxInt16)
+	a.Empty(r.Validate(5))
+	a.Empty(r.Validate(5.1))
+	a.Empty(r.Validate(math.MaxInt8))
+	a.Equal(r.Validate(math.MaxInt32), "msg")
+	a.Equal(r.Validate(-1), "msg")
+	a.Equal(r.Validate(-1.1), "msg")
+	a.Equal(r.Validate("5"), "invalid-type")
 }
