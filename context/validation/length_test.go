968eedb18f44d353dd38e1e13cb1150c8b73edce
caixw <xnotepad@gmail.com>
2020-10-28 23:14:10 +0800

    feat(context/validation): 添加 Min、Max、MinLength 和 MaxLength

--- context/validation/length_test.go
+++ context/validation/length_test.go
@@ -12,17 +12,31 @@ import (
 func TestLength(t *testing.T) {
 	a := assert.New(t)
 
+	a.Panic(func() {
+		Length("msg", "invalid-type", -1, -1)
+	})
+
 	l := Length("msg", "invalid-type", 5, 7)
 	a.Equal(l.Validate("123"), "msg")
 	a.Equal(l.Validate([]byte("123")), "msg")
 	a.Empty(l.Validate([]rune("12345")))
 	a.Equal(l.Validate(&struct{}{}), "invalid-type")
+
+	l = MinLength("msg", "invalid", 6)
+	a.Empty(l.Validate("123456"))
+	a.Empty(l.Validate("12345678910"))
+	a.Equal(l.Validate("12345"), "msg")
+
+	l = MaxLength("msg", "invalid", 6)
+	a.Empty(l.Validate("123456"))
+	a.Equal(l.Validate("12345678910"), "msg")
+	a.Empty(l.Validate("12345"))
 }
 
 func TestRange(t *testing.T) {
 	a := assert.New(t)
 
-	r := Range("msg", "invali-type", 5, math.MaxInt16)
+	r := Range("msg", "invalid-type", 5, math.MaxInt16)
 	a.Empty(r.Validate(5))
 	a.Empty(r.Validate(5.1))
 	a.Empty(r.Validate(math.MaxInt8))
@@ -30,4 +44,14 @@ func TestRange(t *testing.T) {
 	a.Equal(r.Validate(-1), "msg")
 	a.Equal(r.Validate(-1.1), "msg")
 	a.Equal(r.Validate("5"), "invalid-type")
+
+	r = Min("msg", "invalid", 6)
+	a.Empty(r.Validate(6))
+	a.Empty(r.Validate(10))
+	a.Equal(r.Validate(5), "msg")
+
+	r = Max("msg", "invalid", 6)
+	a.Empty(r.Validate(6))
+	a.Equal(r.Validate(10), "msg")
+	a.Empty(r.Validate(5))
 }
