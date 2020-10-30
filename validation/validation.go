611f449d86c4d4a880ee0142a0f1c2c407c14852
caixw <xnotepad@gmail.com>
2020-10-30 15:06:53 +0800

    feat(validation): 添加 Validation.NewObject 方法

--- validation/validation.go
+++ validation/validation.go
@@ -17,6 +17,7 @@ type ErrorHandling int8
 
 // Validation 验证器
 type Validation struct {
+	ctx           *context.Context
 	errorHandling ErrorHandling
 	errors        context.ResultFields
 }
@@ -24,17 +25,32 @@ type Validation struct {
 // New 返回新的 Validation 实例
 //
 // exitAtError 当验证器返回错误时，是否直接中断验证。
-func New(errorHandling ErrorHandling) *Validation {
+func New(ctx *context.Context, errorHandling ErrorHandling) *Validation {
 	return &Validation{
+		ctx:           ctx,
 		errorHandling: errorHandling,
 		errors:        context.ResultFields{},
 	}
+}
+
+// NewObject 验证对象内的数据
+func (v *Validation) NewObject(validator context.Validator, name string) *Validation {
+	if errors := validator.Validate(v.ctx); len(errors) > 0 {
+		for key, vals := range errors {
+			v.errors.Add(name+key, vals...)
+		}
+	}
+	return v
 }
 
 // NewField 验证新的字段
 func (v *Validation) NewField(val interface{}, name string, rules ...Ruler) *Validation {
 	if len(v.errors) > 0 && v.errorHandling == ExitAtError {
 		return v
+	}
+
+	if vv, ok := val.(context.Validator); ok {
+		return v.NewObject(vv, name)
 	}
 
 	for _, rule := range rules {
@@ -50,7 +66,7 @@ func (v *Validation) NewField(val interface{}, name string, rules ...Ruler) *Val
 	return v
 }
 
-// Result 返回结果
+// Result 返回验证结果
 func (v *Validation) Result() context.ResultFields {
 	return v.errors
 }
