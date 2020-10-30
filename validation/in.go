342b4a83df104eb8bbfc4a7f58ee80b3dbc09a0d
caixw <xnotepad@gmail.com>
2020-10-31 00:57:45 +0800

    refactor(validation): 合并 inRule 和 notInRule

--- validation/in.go
+++ validation/in.go
@@ -11,42 +11,34 @@ import (
 type inRule struct {
 	elements []interface{}
 	msg      string
+	not      bool
 }
-
-type notInRule inRule
 
 // In 声明枚举类型的验证规则
 //
 // 要求验证的值必须包含在 element 元素中，如果不存在，则返回 msg 的内容。
 func In(msg string, element ...interface{}) Ruler {
-	return &inRule{
-		msg:      msg,
-		elements: element,
-	}
+	return newInRule(false, msg, element...)
 }
 
 // NotIn 声明不在枚举中的验证规则
-//
-// 正好与 In 规则相反。
 func NotIn(msg string, element ...interface{}) Ruler {
-	return &notInRule{
+	return newInRule(true, msg, element...)
+}
+
+func newInRule(not bool, msg string, element ...interface{}) Ruler {
+	return &inRule{
 		msg:      msg,
 		elements: element,
+		not:      not,
 	}
 }
 
-func (rule *inRule) Validate(v interface{}) (msg string) {
-	if !rule.in(v) {
+func (rule *inRule) Validate(v interface{}) string {
+	if (rule.not && rule.in(v)) || (!rule.not && !rule.in(v)) {
 		return rule.msg
 	}
-	return
-}
-
-func (rule *notInRule) Validate(v interface{}) (msg string) {
-	if r := (*inRule)(rule); r.in(v) {
-		return rule.msg
-	}
-	return
+	return ""
 }
 
 func (rule *inRule) in(v interface{}) bool {
