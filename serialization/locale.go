c662400963973e5291aee6c228a21ac948bf125b
caixw <xnotepad@gmail.com>
2021-08-17 17:46:28 +0800

    feat(serialization): 添加 Locale
    从 internal/locale 移至 serialization，用于从文件中加载本地化内容。

---
+++ serialization/locale.go
@@ -0,0 +1,170 @@
+// SPDX-License-Identifier: MIT
+
+package serialization
+
+import (
+	"io/fs"
+	"path/filepath"
+
+	"golang.org/x/text/feature/plural"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
+	"golang.org/x/text/message/catalog"
+)
+
+type (
+	// Locale 提供了从文件中加载本地化信息的方法
+	Locale struct {
+		b *catalog.Builder
+		f *Files
+	}
+
+	// TagBuilder 简化 catalog.Builder
+	//
+	// 为 catalog.Builder.SetString 省略了 tag 参数
+	TagBuilder struct {
+		builder *catalog.Builder
+		tag     language.Tag
+	}
+
+	localeMessages struct {
+		XMLName  struct{}        `xml:"messages" json:"-" yaml:"-"`
+		Language language.Tag    `xml:"language,attr" json:"language" yaml:"language"`
+		Messages []localeMessage `xml:"message" json:"messages" yaml:"messages"`
+	}
+
+	// localeMessage 单条消息
+	localeMessage struct {
+		Key     string     `xml:"key" json:"key" yaml:"key"`
+		Message localeText `xml:"message" json:"message" yaml:"message"`
+	}
+
+	localeText struct {
+		Msg    string        `xml:"msg,omitempty" json:"msg,omitempty"  yaml:"msg,omitempty"`
+		Select *localeSelect `xml:"select,omitempty" json:"select,omitempty" yaml:"select,omitempty"`
+		Vars   []*localeVar  `xml:"var,omitempty" json:"vars,omitempty" yaml:"vars,omitempty"`
+	}
+
+	localeSelect struct {
+		Arg    int               `xml:"arg,attr" json:"arg" yaml:"arg"`
+		Format string            `xml:"format,attr,omitempty" json:"format,omitempty" yaml:"format,omitempty"`
+		Cases  map[string]string `xml:"case" json:"cases" yaml:"cases"` // TODO 必须偶数
+	}
+
+	localeVar struct {
+		Name   string        `xml:"name" json:"name" yaml:"name"`
+		Select *localeSelect `xml:"select" json:"select" yaml:"select"`
+	}
+)
+
+func (s *localeSelect) message() catalog.Message {
+	cases := make([]interface{}, 0, len(s.Cases))
+	for k, v := range s.Cases {
+		cases = append(cases, k, v)
+	}
+	return plural.Selectf(s.Arg, s.Format, cases...)
+}
+
+func (v *localeVar) message() catalog.Message {
+	return catalog.Var(v.Name, v.Select.message())
+}
+
+func NewLocale(b *catalog.Builder, f *Files) *Locale {
+	return &Locale{b: b, f: f}
+}
+
+func (b *TagBuilder) SetString(key, msg string) error {
+	return b.builder.SetString(b.tag, key, msg)
+}
+
+func (b *TagBuilder) Set(key string, msg ...catalog.Message) error {
+	return b.builder.Set(b.tag, key, msg...)
+}
+
+func (b *TagBuilder) SetMacro(key string, msg ...catalog.Message) error {
+	return b.builder.SetMacro(b.tag, key, msg...)
+}
+
+// Builder 返回本地化操作的相关接口
+func (l *Locale) Builder() *catalog.Builder { return l.b }
+
+func (l *Locale) TagBuilder(tag language.Tag) *TagBuilder {
+	return &TagBuilder{
+		builder: l.Builder(),
+		tag:     tag,
+	}
+}
+
+func (l *Locale) Printer(tag language.Tag) *message.Printer {
+	return message.NewPrinter(tag, message.Catalog(l.Builder()))
+}
+
+// LoadFile 从文件中加载本地化内容
+func (l *Locale) LoadFile(glob string) error {
+	matchs, err := filepath.Glob(glob)
+	if err != nil {
+		return err
+	}
+
+	for _, f := range matchs {
+		m := &localeMessages{}
+		if err := l.f.Load(f, m); err != nil {
+			return err
+		}
+
+		if err := l.set(m); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// LoadFileFS 从文件中加载本地化内容
+func (l *Locale) LoadFileFS(fsys fs.FS, glob string) error {
+	matchs, err := fs.Glob(fsys, glob)
+	if err != nil {
+		return err
+	}
+
+	for _, f := range matchs {
+		m := &localeMessages{}
+		if err := l.f.LoadFS(fsys, f, m); err != nil {
+			return err
+		}
+
+		if err := l.set(m); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (l *Locale) set(m *localeMessages) (err error) {
+	tag := l.TagBuilder(m.Language)
+
+	for _, msg := range m.Messages {
+		switch {
+		case msg.Message.Vars != nil:
+			vars := msg.Message.Vars
+			msgs := make([]catalog.Message, 0, len(vars))
+			for _, v := range vars {
+				msgs = append(msgs, v.message())
+			}
+			msgs = append(msgs, catalog.String(msg.Message.Msg))
+			err = tag.Set(msg.Key, msgs...)
+		case msg.Message.Select != nil:
+			s := msg.Message.Select
+			err = tag.Set(msg.Key, s.message())
+		case msg.Message.Msg != "":
+			err = tag.SetString(msg.Key, msg.Message.Msg)
+		}
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
---
+++ serialization/locale_test.go
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+
+package serialization
+
+import (
+	"encoding/xml"
+	"testing"
+
+	"github.com/issue9/assert"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message/catalog"
+	"gopkg.in/yaml.v2"
+)
+
+func TestLocale_LoadFile(t *testing.T) {
+	a := assert.New(t)
+	f := NewFiles(10)
+	l := NewLocale(catalog.NewBuilder(), f)
+	a.NotNil(l)
+
+	a.ErrorString(l.LoadFile("./testdata/*.yaml"), "未找到适合")
+
+	f.Add(xml.Marshal, xml.Unmarshal, ".xml")
+	f.Add(yaml.Marshal, yaml.Unmarshal, ".yaml")
+
+	a.NotError(l.LoadFile("./testdata/*.yaml"))
+	p := l.Printer(language.MustParse("cmn-hans"))
+
+	a.Equal(p.Sprintf("k1"), "msg1")
+	a.Equal(p.Sprintf("k2", 1), "msg-1")
+	a.Equal(p.Sprintf("k2", 3), "msg-3")
+	a.Equal(p.Sprintf("k2", 5), "msg-other")
+}
