729e1400d4c1f43600732225945a82729c1d1c1c
caixw <xnotepad@gmail.com>
2021-08-19 02:11:41 +0800

    refactor: 将 Command 移到根目录

---
+++ cmd.go
@@ -0,0 +1,143 @@
+// SPDX-License-Identifier: MIT
+
+package web
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"os/signal"
+	"time"
+
+	"golang.org/x/text/message/catalog"
+	"gopkg.in/yaml.v2"
+
+	"github.com/issue9/web/content"
+	"github.com/issue9/web/serialization"
+	"github.com/issue9/web/server"
+)
+
+// Command 提供一种简单的命令行处理方式
+type Command struct {
+	// 程序名称
+	Name string
+
+	// 程序版本
+	Version string
+
+	// 当作服务运行的标签名
+	ServeTag string
+
+	// 退出的信号
+	//
+	// 为 nil 表示没有，如果是 []，则表示所有信息。
+	Signals []os.Signal
+
+	CmdVersion string
+	CmdTag     string
+	CmdFS      string
+
+	// 信息的输出通道
+	//
+	// 可以 os.Stdout 和 os.Stderr 选择，默认为 os.Stdout。
+	Out io.Writer
+
+	// 以下是初始 Server 对象的参数
+
+	ResultBuilder content.BuildResultFunc // 默认为 content.DefaultBuilder
+	Locale        *serialization.Locale   // 默认情况下，能正常解析 xml、yaml 和 json
+	LogsFilename  string                  // 默认为 logs.xml
+	WebFilename   string                  // 默认为 web.yaml
+}
+
+// Exec 执行命令行操作
+func (cmd *Command) Exec() {
+	if cmd.Out == nil {
+		cmd.Out = os.Stdout
+	}
+
+	if cmd.Locale == nil {
+		l := serialization.NewLocale(catalog.NewBuilder(), serialization.NewFiles(5))
+
+		if err := l.Files().Add(json.Marshal, json.Unmarshal, ".json"); err != nil {
+			panic(err)
+		}
+
+		if err := l.Files().Add(xml.Marshal, xml.Unmarshal, ".xml"); err != nil {
+			panic(err)
+		}
+
+		if err := l.Files().Add(yaml.Marshal, yaml.Unmarshal, ".yaml", ".yml"); err != nil {
+			panic(err)
+		}
+	}
+
+	if cmd.LogsFilename == "" {
+		cmd.LogsFilename = "logs.xml"
+	}
+	if cmd.WebFilename == "" {
+		cmd.WebFilename = "web.yaml"
+	}
+
+	if cmd.CmdFS == "" {
+		cmd.CmdFS = "fs"
+	}
+	if cmd.CmdTag == "" {
+		cmd.CmdTag = "tag"
+	}
+	if cmd.CmdVersion == "" {
+		cmd.CmdVersion = "v"
+	}
+
+	if err := cmd.exec(); err != nil {
+		panic(err)
+	}
+}
+
+func (cmd *Command) exec() error {
+	v := flag.Bool(cmd.CmdVersion, false, "显示版本号")
+	tag := flag.String(cmd.CmdTag, "", "执行的标签")
+	f := flag.String(cmd.CmdFS, "./", "可读取的目录")
+	dir := os.DirFS(*f)
+
+	if *v {
+		fmt.Fprintln(cmd.Out, cmd.Name, cmd.Version)
+		return nil
+	}
+
+	srv, err := LoadServer(cmd.Name, cmd.Version, cmd.ResultBuilder, cmd.Locale, dir, cmd.LogsFilename, cmd.WebFilename)
+	if err != nil {
+		return err
+	}
+
+	if err := srv.InitModules(*tag); err != nil {
+		return err
+	}
+	if *tag == cmd.ServeTag {
+		if cmd.Signals != nil { // nil 和 [] 处理方式不同，[] 表示所有信息
+
+			cmd.grace(srv, cmd.Signals...)
+		}
+		return srv.Serve()
+	}
+	return nil
+}
+
+func (cmd *Command) grace(s *server.Server, sig ...os.Signal) {
+	go func() {
+		signalChannel := make(chan os.Signal, 1)
+		signal.Notify(signalChannel, sig...)
+
+		<-signalChannel
+		signal.Stop(signalChannel)
+		close(signalChannel)
+
+		if err := s.Close(3 * time.Second); err != nil {
+			s.Logs().Error(err)
+		}
+		s.Logs().Flush() // 保证内容会被正常输出到日志。
+	}()
+}
