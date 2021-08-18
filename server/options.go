4ed54986d1c17c5b3b0ab97297d6e155c10a9a61
caixw <xnotepad@gmail.com>
2021-08-19 02:19:59 +0800

    chore: 提升版本号

--- server/options.go
+++ server/options.go
@@ -60,7 +60,7 @@ type Options struct {
 
 	// 可以对 http.Server 的内容进行修改
 	//
-	// NOTE: 对 http.Server.Handler 的修改不会启作用，该值始终会指向 Server.mux
+	// NOTE: 对 http.Server.Handler 的修改不会启作用，该值始终会指向 Server.groups
 	HTTPServer func(*http.Server)
 	httpServer *http.Server
 
--- web.go
+++ web.go
@@ -4,4 +4,4 @@
 package web
 
 // Version 当前框架的版本
-const Version = "0.40.0"
+const Version = "0.41.0"
