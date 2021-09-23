1b01f6b44b2823b495fa4ad57e541be13fd33a11
caixw <xnotepad@gmail.com>
2021-09-24 02:53:30 +0800

    test(server): 添加缺失的测试用例

--- server/module_test.go
+++ server/module_test.go
@@ -20,27 +20,30 @@ var _ fs.FS = &Module{}
 func TestServer_initModules(t *testing.T) {
 	a := assert.New(t)
 	s := newServer(a)
+	var count int
 
 	m1, err := s.NewModule("users1", "1.0.0", localeutil.Phrase("user1 module"), "users2", "users3")
 	a.NotNil(m1).NotError(err)
-	t1 := m1.Action("v1")
-	a.NotNil(t1)
-	t1.AddInit("安装数据表 users1", func() error { return errors.New("failed message") })
-	t1.AddUninit("安装数据表 users1", func() error { return nil })
+	m1.AddInit("init m1", func() error { count++; return nil })   // count++
+	m1.AddUninit("init m1", func() error { count--; return nil }) // count--
+	v1 := m1.Action("v1")
+	a.NotNil(v1)
+	v1.AddInit("安装数据表 users1", func() error { return errors.New("failed message") })
+	v1.AddUninit("安装数据表 users1", func() error { return nil })
 
 	m2, err := s.NewModule("users2", "1.0.0", localeutil.Phrase("user2 module"), "users3")
 	a.NotNil(m2).NotError(err)
-	t2 := m2.Action("v2")
-	a.NotNil(t2)
-	t2.AddInit("安装数据表 users2", func() error { return nil })
+	v2 := m2.Action("v2")
+	a.NotNil(v2)
+	v2.AddInit("安装数据表 users2", func() error { count++; return nil }) // count++
 	m2.Action("v3")
 
 	m3, err := s.NewModule("users3", "1.0.0", localeutil.Phrase("user3 module"))
 	a.NotNil(m3).NotError(err)
-	action := m3.Action("v2")
-	a.NotNil(action)
-	action.AddInit("安装数据表 users3-1", func() error { return nil })
-	action.AddInit("安装数据表 users3-2", func() error { return nil })
+	v2 = m3.Action("v2")
+	a.NotNil(v2)
+	v2.AddInit("安装数据表 users3-1", func() error { count++; return nil }) // count++
+	v2.AddInit("安装数据表 users3-2", func() error { count++; return nil }) // count++
 	a.NotNil(m3.Action("v4"))
 
 	actions := s.Actions()
@@ -49,26 +52,23 @@ func TestServer_initModules(t *testing.T) {
 	a.Panic(func() {
 		s.initModules("", false) // 空值
 	})
-	a.ErrorString(s.initModules("v1", false), "failed message")
-	a.NotError(s.initModules("v1", true))
-	a.NotError(s.initModules("v2", false))
-	a.NotError(s.initModules("not-exists", false))
 
-	// TODO
+	a.Equal(0, count)
+	a.ErrorString(s.initModules("v1", false), "failed message") // 这里执行了一次 m1.inits 中的 count++
+
+	a.Equal(1, count)
+	a.NotError(s.initModules("v1", true)) // 这里执行了一次 m1.uninits 中的 count--
+	a.Equal(0, count)
+
+	a.NotError(s.initModules("v2", false)) // 包括了 m1.inits 中的 count++
+	a.Equal(4, count)
+	a.NotError(s.initModules("not-exists", false))
 }
 
 func TestPluginInitFuncName(t *testing.T) {
 	a := assert.New(t)
 
 	a.True(unicode.IsUpper(rune(PluginInitFuncName[0])))
-}
-
-func TestNewModule(t *testing.T) {
-	a := assert.New(t)
-	s := newServer(a)
-
-	m, err := s.NewModule("id", "1.0.0", localeutil.Phrase("desc"), "id1", "id2")
-	a.NotNil(m).NotError(err)
 }
 
 func TestServer_Actions(t *testing.T) {
@@ -115,7 +115,6 @@ func TestModule_Action(t *testing.T) {
 func TestServer_NewModule(t *testing.T) {
 	a := assert.New(t)
 	srv := newServer(a)
-	a.NotNil(srv)
 
 	builder := catalog.NewBuilder()
 	a.NotError(builder.SetString(language.SimplifiedChinese, "m1 desc", "m1 描述信息"))
@@ -136,7 +135,7 @@ func TestServer_NewModule(t *testing.T) {
 	})
 }
 
-func TestActioon_AddInit(t *testing.T) {
+func TestAction_AddInit(t *testing.T) {
 	a := assert.New(t)
 	s := newServer(a)
 	m, err := s.NewModule("m1", "1.0.0", localeutil.Phrase("m1 desc"))
@@ -151,4 +150,32 @@ func TestActioon_AddInit(t *testing.T) {
 	a.Equal(tag.inits[2].Title, "2")
 	a.Equal(tag.Module(), m)
 	a.Equal(tag.Server(), s)
+}
+
+func TestModule_Object(t *testing.T) {
+	a := assert.New(t)
+	s := newServer(a)
+	o := 5
+
+	m1, err := s.NewModule("m1", "1.0.0", localeutil.Phrase("m1 desc"))
+	a.NotError(err).NotNil(m1)
+	m1.AttachObject(o)
+	a.Equal(m1.Object(), o)
+
+	m2, err := s.NewModule("m2", "1.0.0", localeutil.Phrase("m2 desc"), "m1")
+	a.NotError(err).NotNil(m2)
+	a.Equal(m2.DepObject("m1"), o)
+	a.PanicString(func() {
+		m2.DepObject("not-dep")
+	}, "的依赖对象")
+	a.PanicString(func() {
+		m1.DepObject("m1")
+	}, "的依赖对象")
+
+	// m2 并未指定 Object
+	m3, err := s.NewModule("m3", "1.0.0", localeutil.Phrase("m3 desc"), "not-exists")
+	a.NotError(err).NotNil(m3)
+	a.PanicString(func() {
+		m3.DepObject("not-exists")
+	}, "依赖项 not-exists 未找到")
 }
