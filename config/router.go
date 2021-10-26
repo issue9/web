ac3a9c8e1143e7d006b4f912591f5d9d64be2652
caixw <xnotepad@gmail.com>
2021-10-26 14:48:40 +0800

    refactor(config): 统一字段名称

--- config/router.go
+++ config/router.go
@@ -17,18 +17,18 @@ type Router struct {
 
 // CORS 跨域设置
 type CORS struct {
-	// AllowedOrigins 对应 Origin
+	// Origins 对应 Origin
 	//
 	// 可以是 *，如果包含了 *，那么其它的设置将不再启作用。
 	// 此字段将被用于与请求头的 Origin 字段作验证，以确定是否放行该请求。
 	//
 	// 如果此值为空，表示不启用跨域的相关设置。
-	AllowedOrigins []string `yaml:"origins,omitempty" json:"origins,omitempty" xml:"origins,omitempty"`
+	Origins []string `yaml:"origins,omitempty" json:"origins,omitempty" xml:"origin,omitempty"`
 
-	// AllowedHeaders 对应 Access-Control-Allow-Headers
+	// AllowHeaders 对应 Access-Control-Allow-Headers
 	//
 	// 可以包含 *，表示可以是任意值，其它值将不再启作用。
-	AllowedHeaders []string `yaml:"allowedHeaders,omitempty" json:"allowedHeaders,omitempty" xml:"allowedHeader,omitempty"`
+	AllowHeaders []string `yaml:"allowHeaders,omitempty" json:"allowHeaders,omitempty" xml:"allowHeader,omitempty"`
 
 	// ExposedHeaders 对应 Access-Control-Expose-Headers
 	ExposedHeaders []string `yaml:"exposedHeaders,omitempty" json:"exposedHeaders,omitempty" xml:"exposedHeader,omitempty"`
@@ -40,7 +40,7 @@ type CORS struct {
 	// 其它 >= -1 的值正常输出数值；
 	MaxAge int `yaml:"maxAge,omitempty" json:"maxAge,omitempty" xml:"maxAge,attr,omitempty"`
 
-	// AllowCredentials 是否允许 cookie
+	// AllowCredentials 对应 Access-Control-Allow-Credentials
 	AllowCredentials bool `yaml:"allowCredentials,omitempty" json:"allowCredentials,omitempty" xml:"allowCredentials,attr,omitempty"`
 }
 
@@ -56,8 +56,8 @@ func (r *Router) sanitize() *Error {
 	}
 
 	r.cors = &mux.CORS{
-		AllowedOrigins:   r.CORS.AllowedOrigins,
-		AllowedHeaders:   r.CORS.AllowedHeaders,
+		Origins:          r.CORS.Origins,
+		AllowHeaders:     r.CORS.AllowHeaders,
 		ExposedHeaders:   r.CORS.ExposedHeaders,
 		MaxAge:           r.CORS.MaxAge,
 		AllowCredentials: r.CORS.AllowCredentials,
@@ -67,7 +67,7 @@ func (r *Router) sanitize() *Error {
 }
 
 func (c *CORS) sanitize() *Error {
-	for _, o := range c.AllowedOrigins {
+	for _, o := range c.Origins {
 		if o == "*" {
 			if c.AllowCredentials {
 				return &Error{Field: "allowCredentials", Message: "不能与 allowedOrigins=* 同时成立"}
