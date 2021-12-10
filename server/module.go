e8cbd9ab3730efdce8e77a6ad57af8828c0c50d5
caixw <xnotepad@gmail.com>
2021-12-10 18:13:39 +0800

    docs(server): 添加文档

--- server/module.go
+++ server/module.go
@@ -6,6 +6,7 @@ import (
 	"io/fs"
 
 	"github.com/issue9/sliceutil"
+
 	"github.com/issue9/web/internal/filesystem"
 )
 
@@ -46,8 +47,14 @@ func (srv *Server) NewModule(id string) *Module {
 
 func (m *Module) Server() *Server { return m.srv }
 
+// AddFS 添加文件系统
+//
+// Module 默认以 id 为名称相对于 Server 创建了一个文件系统，
+// 此操作会将 fsys 作为 Module 另一个文件系统与 Module 相关联，
+// 当执行 Open 等操作时，会依然以关联顺序查找相应的文件系统， 直到找到。
 func (m *Module) AddFS(fsys ...fs.FS) { m.fs.Add(fsys...) }
 
+// ID 模块的唯一 ID
 func (m *Module) ID() string { return m.id }
 
 func (m *Module) Open(name string) (fs.File, error) { return m.fs.Open(name) }
