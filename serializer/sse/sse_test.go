a2dd252ea2eee8e43410aa1638bbb9c76ad037ef
caixw <xnotepad@gmail.com>
2023-09-22 16:34:56 +0800

    test(serializer/sse): 调整 TestSSE 中的等待关闭服务的时间

--- serializer/sse/sse_test.go
+++ serializer/sse/sse_test.go
@@ -13,6 +13,7 @@ import (
 
 	"github.com/issue9/web"
 	"github.com/issue9/web/logs"
+	"github.com/issue9/web/serializer/json"
 	"github.com/issue9/web/servertest"
 )
 
@@ -20,14 +21,20 @@ func TestSSE(t *testing.T) {
 	a := assert.New(t, false)
 	s, err := web.NewServer("test", "1.0.0", &web.Options{
 		HTTPServer: &http.Server{Addr: ":8080"},
-		Mimetypes:  []*web.Mimetype{{Type: Mimetype}},
-		Logs:       &logs.Options{Handler: logs.NewTermHandler(logs.MicroLayout, os.Stderr, nil)},
+		Mimetypes: []*web.Mimetype{
+			{Type: "application/json", Marshal: json.Marshal, Unmarshal: json.Unmarshal},
+		},
+		Logs: &logs.Options{
+			Handler: logs.NewTermHandler(logs.MicroLayout, os.Stderr, nil),
+			Caller:  true,
+			Created: true,
+			Levels:  logs.AllLevels(),
+		},
 	})
 	a.NotError(err).NotNil(s)
 	e := New[int64](s, http.StatusCreated, time.Minute, time.Minute)
 	a.NotNil(e)
-
-	s.NewRouter("def", nil).Get("/events/{id}", func(ctx *web.Context) web.Responser {
+	s.NewRouter("default", nil).Get("/event/{id}", func(ctx *web.Context) web.Responser {
 		id, resp := ctx.PathInt64("id", web.ProblemBadRequest)
 		if resp != nil {
 			return resp
@@ -49,14 +56,14 @@ func TestSSE(t *testing.T) {
 	defer servertest.Run(a, s)()
 	a.Equal(0, e.Len())
 
-	time.AfterFunc(5000*time.Microsecond, func() {
+	time.AfterFunc(500*time.Millisecond, func() {
 		a.Nil(e.Get(100))
-		a.NotNil(e.Get(5))
-		s.Close(500 * time.Microsecond)
+		a.NotNil(e.Get(5)) // 需要有人访问 /events/{id} 之后才有值
+		s.Close(500 * time.Millisecond)
 	})
 
-	servertest.Get(a, "http://localhost:8080/events/5").
-		Header("accept", Mimetype).
+	servertest.Get(a, "http://localhost:8080/event/5").
+		Header("accept", "application/json").
 		Header("accept-encoding", "").
 		Do(nil).
 		Status(http.StatusCreated).
