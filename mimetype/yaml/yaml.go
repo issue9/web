74b130bd4c4a4ac804b6794d83c03b9b555e80af
caixw <xnotepad@gmail.com>
2024-11-13 00:01:23 +0800

    feat(mimetype/yaml): 支持 YAML 编码

---
+++ mimetype/yaml/yaml.go
@@ -0,0 +1,23 @@
+// SPDX-FileCopyrightText: 2024 caixw
+//
+// SPDX-License-Identifier: MIT
+
+// Package yaml 支持 YAML 编码的序列化操作
+package yaml
+
+import (
+	"io"
+
+	"gopkg.in/yaml.v3"
+
+	"github.com/issue9/web"
+)
+
+const (
+	Mimetype        = "application/yaml"
+	ProblemMimetype = "application/problem+yaml"
+)
+
+func Marshal(_ *web.Context, v any) ([]byte, error) { return yaml.Marshal(v) }
+
+func Unmarshal(r io.Reader, v any) error { return yaml.NewDecoder(r).Decode(v) }
