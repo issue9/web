f6730490846c1a205fb764dd96740867d5e2a475
caixw <xnotepad@gmail.com>
2025-02-13 09:29:46 +0800

    refactor(openapi): 将 a.go 并入其它文件

--- openapi/build.go
+++ openapi/build.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2024 caixw
+// SPDX-FileCopyrightText: 2024-2025 caixw
 //
 // SPDX-License-Identifier: MIT
 
@@ -17,6 +17,21 @@ import (
 
 	"github.com/issue9/web"
 )
+
+// 此对象不能和 operationRenderer 放在同一文件，且需要在其之前编译，
+// 否则会和造成泛型对象相互引用，无法编译。
+type pathItemRenderer struct {
+	Get        *operationRenderer             `json:"get,omitempty" yaml:"get,omitempty"`
+	Put        *operationRenderer             `json:"put,omitempty" yaml:"put,omitempty"`
+	Post       *operationRenderer             `json:"post,omitempty" yaml:"post,omitempty"`
+	Delete     *operationRenderer             `json:"delete,omitempty" yaml:"delete,omitempty"`
+	Options    *operationRenderer             `json:"options,omitempty" yaml:"options,omitempty"`
+	Head       *operationRenderer             `json:"head,omitempty" yaml:"head,omitempty"`
+	Patch      *operationRenderer             `json:"patch,omitempty" yaml:"patch,omitempty"`
+	Trace      *operationRenderer             `json:"trace,omitempty" yaml:"trace,omitempty"`
+	Servers    []*serverRenderer              `json:"servers,omitempty" yaml:"servers,omitempty"`
+	Parameters []*renderer[parameterRenderer] `json:"parameters,omitempty" yaml:"parameters,omitempty"`
+}
 
 func (d *Document) build(p *message.Printer, lang language.Tag, filterTags []string) *openAPIRenderer {
 	servers := make([]*serverRenderer, 0, len(d.servers))
